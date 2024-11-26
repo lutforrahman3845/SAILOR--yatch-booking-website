9d6ac598035d153b576ef022fec35f508d30a97c
Lutfor Rahman <lutforr3845@gmail.com>
2024-11-27 01:25:23 +0600

    finsh seasonal_offer and Blogs section

--- dist/js/script.js
+++ dist/js/script.js
@@ -30,3 +30,69 @@ modal.addEventListener("close", () => {
   iframe.src = "";
   iframe.src = "https://www.youtube.com/embed/RktXtegZvNw?si=2-lA-AN0EkWUQTo0";
 });
+
+// swiper
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  initialSlide: 2,
+  speed: 600,
+  preventClicks: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 80,
+    depth: 350,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".next",
+    prevEl: ".previous"
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+    },
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+  on: {
+    slideChange: function () {
+      updateActiveSlideTitle();
+    },
+    transitionEnd: function () {
+      updateActiveSlideTitle();
+    },
+  },
+});
+
+function updateActiveSlideTitle() {
+  const slides = document.querySelectorAll(".swiper-slide");
+  slides.forEach((slide) => {
+    const title = slide.querySelector(".title");
+    if (slide.classList.contains("swiper-slide-active")) {
+      title.classList.remove("hidden");
+    } else {
+      title.classList.add("hidden");
+    }
+  });
+}
+updateActiveSlideTitle();
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,12 +14,21 @@ module.exports = {
       },
       backgroundImage: {
         'hero-image': "url('../assets/bannar.png')",
+        'hero-pattern': "url('../assets/Rectangle 1630.png')",
       },
       animation: {
         'spin-slow': 'spin 6s linear infinite',
       },
       boxShadow: { 
         'custom-white': '0.313rem 0.313rem 0 0.313rem #fff', 
+        'slider-shadow':'-0.313rem -0.313rem 0 0.25rem #fff',
+        'sm-slider-shadow':'-0.313rem -0.313rem 0 0.1rem #fff',
+        'slider-shadow-2':'-0.313rem 0.313rem 0 0.25rem #fff',
+        'sm-slider-shadow-2':'-0.313rem 0.313rem 0 0.1rem #fff',
+        'slider-shadow-3':'0.313rem -0.313rem 0 0.25rem #fff',
+        'sm-slider-shadow-3':'0.313rem -0.313rem 0 0.1rem #fff',
+        'slider-shadow-4':'0.313rem 0.313rem 0 0.25rem #fff',
+        'sm-slider-shadow-4':'0.313rem 0.313rem 0 0.1rem #fff',
       },
     },
   },
